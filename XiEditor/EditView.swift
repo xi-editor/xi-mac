cd18ff80c83dacf58a5ad76ebc569b478488f46f
Ryan Bloom <ryan@ryan-bloom.com>
2018-07-20 15:42:36 -0400

    Attempt to fix memory leak

--- XiEditor/EditView.swift
+++ XiEditor/EditView.swift
@@ -134,7 +134,7 @@ class EditView: NSView, NSTextInputClient, TextPlaneDelegate {
     var gutterXPad: CGFloat = 8
     var gutterCache: GutterCache?
 
-    var dataSource: EditViewDataSource!
+    weak var dataSource: EditViewDataSource!
 
     var lastDragLineCol: (Int, Int)?
     var timer: Timer?
