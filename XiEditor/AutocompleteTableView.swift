ee7eff3c0ddcf9229b5a281fa91deeeae672836a
Dzũng Lê <dungleviethoang@gmail.com>
2018-07-25 08:16:11 +0700

    Initial autocomplete work

---
+++ XiEditor/AutocompleteTableView.swift
@@ -0,0 +1,17 @@
+//
+//  AutocompleteTableView.swift
+//  XiEditor
+//
+//  Created by Dzung on 25/07/2018.
+//  Copyright © 2018 Raph Levien. All rights reserved.
+//
+
+import Cocoa
+
+class AutocompleteTableView: NSTableView {
+
+}
+
+class AutocompleteTableRowView: NSTableRowView {
+
+}
---
+++ XiEditor/AutocompleteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AutocompleteViewController.swift
+//  XiEditor
+//
+//  Created by Dzũng Lê on 7/24/18.
+//  Copyright © 2018 Raph Levien. All rights reserved.
+//
+
+import Cocoa
+
+class AutocompleteViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+}
