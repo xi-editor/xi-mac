c00d14f6f86683bfa28356d8ee7e4f02b4dec10c
Jan Solanti <jhs@psonet.com>
2018-09-20 01:34:58 +0900

    Correct sRGB conversion formula for rendering

--- XiEditor/XiTextPlane/text.v.glsl
+++ XiEditor/XiTextPlane/text.v.glsl
@@ -33,12 +33,20 @@ uniform vec2 posScale;
 flat out vec4 passColor;
 out vec2 uv;
 
+vec3 to_linear(vec3 srgb) {
+    // 1 if srgb <= 0.04045
+    vec3 selection = step(-0.04045, -srgb);
+    vec3 a = srgb / 12.92;
+    vec3 b = pow((srgb + 0.055) / 1.055, vec3(2.4));
+    // a for components where linear <= 0.04045, b for others
+    return mix(b, a, selection);
+}
+
 void main() {
     vec2 pixelPos = rectOrigin + position * rectSize;
     vec2 pos = pixelPos * posScale + vec2(-1.0, 1.0);
     gl_Position = vec4(pos, 0.0, 1.0);
     passColor = rgba * vec4(1.0 / 255.0);
-    // TODO: more precise sRGB formula
-    passColor = passColor * passColor; // do conversion to sRGB
+    passColor.rgb = to_linear(passColor.rgb);
     uv = uvOrigin + position * uvSize;
 }
