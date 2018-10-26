fdae43a05ceb074126b15c7c50b557d7ed3cb0fc
Mateusz Matoszko <mateusz@matoszko.pl>
2018-10-27 00:07:19 +0200

    Be explicit about non-implemented initWithCoder

--- XiEditor/StatusBar.swift
+++ XiEditor/StatusBar.swift
@@ -45,6 +45,7 @@ class StatusItem: NSTextField {
         self.sizeToFit()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -104,6 +105,7 @@ class StatusBar: NSView {
         updateStatusBarVisibility()
     }
 
+    @available(*, unavailable)
     required init?(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
