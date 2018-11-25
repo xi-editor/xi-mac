6e229fbc97f3b7ddce7fecd957991c2f29a24bd0
Anna Scholtz <anna@scholtzan.net>
2018-11-24 18:53:07 -0800

    find_status parsing optional lines parameter

--- Sources/XiEditor/Search.swift
+++ Sources/XiEditor/Search.swift
@@ -433,8 +433,11 @@ extension EditViewController {
 
             if status.first?["lines"] != nil && !(status.first?["lines"] is NSNull) {
                 query?.lines = statusQuery["lines"] as! [Int]
-                for line in (query?.lines)! {
-                    findMarker.append(Marker(line, color: NSColor.yellow))
+
+                if (query?.lines != nil) {
+                    for line in (query?.lines)! {
+                        findMarker.append(Marker(line, color: NSColor.yellow))
+                    }
                 }
             }
         }
