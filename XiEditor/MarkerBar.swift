88bb7a4a14258cae74a52582182a10fe48f23dd7
Anna Scholtz <anna@scholtzan.net>
2018-11-24 18:49:05 -0800

    Cleanup and fix marker positioning

--- XiEditor/MarkerBar.swift
+++ XiEditor/MarkerBar.swift
@@ -30,117 +30,17 @@ class Marker {
     }
 }
 
-//class MarkerBar: NSView, CALayerDelegate {
-//    var markers: [Marker] = []
-//    var backgroundColor: NSColor = NSColor.clear
-//    var parent: EditViewController!
-//
-//    var markerBarWidth: CGFloat = 16.0 {
-//        didSet {
-//            self.needsDisplay = true
-//        }
-//    }
-//
-//    var markerHeight: CGFloat = 1.0 {
-//        didSet {
-//            self.needsDisplay = true
-//        }
-//    }
-//
-//    override init(frame frameRect: NSRect) {
-//        super.init(frame: .zero)
-//        self.translatesAutoresizingMaskIntoConstraints = false
-//        self.needsDisplay = true
-//    }
-//
-//    @available(*, unavailable)
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    func setMarker(_ items: [Marker]) {
-//        markers = items
-//        self.needsDisplay = true
-//    }
-//
-//    override func draw(_ dirtyRect: NSRect) {
-//        super.draw(dirtyRect)
-//
-//        backgroundColor.setFill()
-//        dirtyRect.fill()
-//
-//        let path = NSBezierPath()
-//        path.lineWidth = 1
-//        path.move(to: CGPoint(x: dirtyRect.minX, y: dirtyRect.minY))
-//        path.line(to: CGPoint(x: dirtyRect.maxX, y: dirtyRect.minY))
-//        path.stroke()
-//
-//        for marker in markers {
-//            let totalLines = CGFloat(parent.lines.height)
-//            let visiblesLines = CGFloat(parent.visibleLines.count)
-//            let lineHeight = parent.textMetrics.linespace
-//            let markerBarHeight = CGFloat(min(totalLines, visiblesLines)) * lineHeight
-//
-//            marker.color.setFill()
-//            marker.color.setStroke()
-//            path.lineWidth = markerHeight
-//            path.move(to: CGPoint(x: dirtyRect.minX, y: dirtyRect.maxY - CGFloat(marker.line) / totalLines * markerBarHeight))
-//            path.line(to: CGPoint(x: dirtyRect.maxX, y: dirtyRect.maxY - CGFloat(marker.line) / totalLines * markerBarHeight))
-//            path.stroke()
-//        }
-//    }
-//}
-//
-//
-//class ScrollViewWithMarkerBar: NSScrollView {
-//    var markerBar: MarkerBar!
-//
-//    private func showMarkerBar() {
-//        markerBar.alphaValue = 1.0
-//    }
-//
-//    @objc private func hideMarkerBar() {
-//        NSAnimationContext.runAnimationGroup({_ in
-//            NSAnimationContext.current.duration = 0.5
-//            markerBar.animator().alphaValue = 0.0
-//        }, completionHandler: {})
-//    }
-//
-//    override func scrollWheel(with event: NSEvent) {
-//        super.scrollWheel(with: event)
-//        if event.type == NSEvent.EventType.scrollWheel {
-//            switch event.phase {
-//            case NSEvent.Phase.mayBegin, NSEvent.Phase.began, NSEvent.Phase.changed:
-//                showMarkerBar()
-//            case NSEvent.Phase.cancelled, NSEvent.Phase.ended:
-//                if event.momentumPhase != NSEvent.Phase.changed &&
-//                   event.momentumPhase != NSEvent.Phase.began {
-//                    self.perform(#selector(hideMarkerBar), with: nil, afterDelay: 0.5)
-//                }
-//            default:
-//                if event.momentumPhase == NSEvent.Phase.ended {
-//                    self.perform(#selector(hideMarkerBar), with: nil, afterDelay: 0.5)
-//                }
-//                break
-//            }
-//        }
-//    }
-//}
-
-//interface NSScroller (BWTSPrivate)
-//- (NSRect)_drawingRectForPart:(NSScrollerPart)aPart;
-//end
-
-
 class MarkerBar: NSScroller {
     var markers: [Marker] = []
     var backgroundColor: NSColor = NSColor.red
     var parent: EditViewController!
     var markerLayer = CAShapeLayer()
+    var overlayScrollerLayer: CALayer?
 
     override func viewWillDraw() {
         super.viewWillDraw()
-        self.layer?.sublayers?.last?.addObserver(self, forKeyPath: "opacity", options: .new, context: nil)
+        overlayScrollerLayer = self.layer?.sublayers?.last
+        overlayScrollerLayer?.addObserver(self, forKeyPath: "opacity", options: .new, context: nil)
         self.layer?.addSublayer(markerLayer)
     }
 
@@ -150,7 +50,7 @@ class MarkerBar: NSScroller {
         }
     }
 
-    var markerHeight: CGFloat = 1.0
+    var markerHeight: CGFloat = 0.5
 
     override class var isCompatibleWithOverlayScrollers: Bool {
         return true
@@ -163,27 +63,26 @@ class MarkerBar: NSScroller {
 
     func setMarker(_ items: [Marker]) {
         markers = items
+        drawMarkers()
     }
 
     func drawMarkers() {
-        print("markers")
         let path = CGMutablePath()
         let totalLines = CGFloat(parent.lines.height)
-        let visiblesLines = CGFloat(parent.visibleLines.count)
-        let lineHeight = parent.textMetrics.linespace
-        let markerBarHeight = CGFloat(min(totalLines, visiblesLines)) * lineHeight
+        let markerBarHeight = self.layer?.bounds.height
+        let maxScrollerWidth = self.layer?.bounds.width
 
         for marker in markers {
-            marker.color.setFill()
-            marker.color.setStroke()
+            markerLayer.fillColor = marker.color.cgColor
 
-            markerLayer.fillColor = CGColor.init(red: 255, green: 255, blue: 255, alpha: 1.0)
+            let width = (overlayScrollerLayer?.bounds.width)!
+            let x = maxScrollerWidth! - width
+            let y = ((CGFloat(marker.line) - 1) / totalLines) * markerBarHeight!
 
-            path.addRect(CGRect(x: markerLayer.bounds.minX, y:CGFloat(marker.line) / totalLines * markerBarHeight, width: 15, height: markerHeight))
+            path.addRect(CGRect(x: x, y: y, width: width, height: markerHeight))
             path.closeSubpath()
         }
 
-        markerLayer.strokeColor = CGColor.init(red: 255, green: 255, blue: 255, alpha: 1.0)
         markerLayer.path = path
     }
 }
