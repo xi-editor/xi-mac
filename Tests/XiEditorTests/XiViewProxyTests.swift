4037f98ee89edee2d95d3e003c0430aaa74cd7ac
Mateusz Matoszko <mateusz@matoszko.pl>
2018-12-30 21:06:16 +0100

    Test resize from XiViewProxy

--- Tests/XiEditorTests/XiViewProxyTests.swift
+++ Tests/XiEditorTests/XiViewProxyTests.swift
@@ -20,4 +20,18 @@ import XCTest
 
 class XiViewProxyTests: XCTestCase {
 
+    func testResize() {
+        let asyncCalledExpectation = expectation(description: "Async should be called")
+        let async: XiViewConnection.AsyncRpc = { method, params,_ in
+            XCTAssertEqual("resize", method)
+            let resizeParams = params as! [String: CGFloat]
+            XCTAssertEqual(["width": 23 as CGFloat, "height": 42 as CGFloat], resizeParams)
+            asyncCalledExpectation.fulfill()
+        }
+        let sync: XiViewConnection.SyncRpc = { _,_ in return .ok("not implemented in tests" as AnyObject) }
+        let connection: XiViewProxy = XiViewConnection(asyncRpc: async, syncRpc: sync)
+        connection.resize(size: CGSize(width: 23, height: 42))
+        wait(for: [asyncCalledExpectation], timeout: 1)
+    }
+
 }
