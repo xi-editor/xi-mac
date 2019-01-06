50eb0382fc5ea90467346ef86be812af0428a1ed
Richard L Zarth III <rlziii@icloud.com>
2019-01-06 11:22:03 -0500

    Issue #365 Make the String building more clear for windowTitle.

--- Sources/XiEditor/XiWindowController.swift
+++ Sources/XiEditor/XiWindowController.swift
@@ -15,19 +15,24 @@
 import AppKit.NSWindowController
 
 final class XiWindowController: NSWindowController {
+    let editedIndicator = "•"
+    let debugIndicator = "[Debug]"
+    
     override func windowTitle(forDocumentDisplayName displayName: String) -> String {
-        var editedIndicator = ""
+        var windowTitle = ""
         
         // If there is more than one tab AND the document in question has unsaved changes then show an edited indicator.
         // When there is only one tab the edited indicator is already built into the macOS close (red circle) button.
         if NSDocumentController.shared.documents.count > 1, document?.hasUnautosavedChanges ?? false {
-            editedIndicator = "• "
+            windowTitle += "\(editedIndicator) "
         }
         
         #if DEBUG
-            return "\(editedIndicator)[Debug] \(displayName)"
-        #else
-            return "\(editedIndicator)\(displayName)"
+            windowTitle += "\(debugIndicator) "
         #endif
+        
+        windowTitle += displayName
+        
+        return windowTitle
     }
 }
