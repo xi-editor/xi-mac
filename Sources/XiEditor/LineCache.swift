9215f4ca78c300fa52b8386e8f661568e7c0b5fd
Jesse Armand <jesse@jessearmand.com>
2019-01-20 14:46:21 +0800

    Fixed annotations update type casting

--- Sources/XiEditor/LineCache.swift
+++ Sources/XiEditor/LineCache.swift
@@ -109,7 +109,8 @@ fileprivate class LineCacheState<T>: UnfairLock {
     /// Updates the state by applying a delta. The update format is detailed in the
     /// [xi-core docs](http://xi-editor.github.io/xi-editor/docs/frontend-protocol.html#view-update-protocol).
     func applyUpdate(update: [String: AnyObject]) -> InvalSet {
-        annotations = AnnotationStore(from: (update["annotations"] as! [[String: AnyObject]]))
+        let updateAnnotations = update["annotations"] as? [[String: AnyObject]] ?? []
+        annotations = AnnotationStore(from: updateAnnotations)
 
         let inval = InvalSet()
         guard let ops = update["ops"] else { return inval }
