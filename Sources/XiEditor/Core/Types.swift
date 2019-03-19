7d9677acd5c42f17fb160050718e5e27b0f96d80
Jeremy Wiebe <jeremy@khanacademy.org>
2019-03-18 20:01:33 -0700

    Introduce UpdateOperation

--- Sources/XiEditor/Core/Types.swift
+++ Sources/XiEditor/Core/Types.swift
@@ -27,7 +27,7 @@ enum UpdateOperationType: String {
 struct UpdateParams {
 	// We elide the intermediate "update" part of the JSON struct
 	let annotations: [[String: Any]]
-	let ops: [[String: Any]]
+	let ops: [UpdateOperation]
 	let pristine: Bool
 
 	init?(fromJson json: [String: Any]) {
@@ -41,7 +41,52 @@ struct UpdateParams {
 		}
 
 		self.annotations = annotations
-		self.ops = ops
+        // STOPSHIP (jeremy): Should we throw/assert if any of these return nil? Probably yes especially for DEBUG
+        self.ops = ops.compactMap { opJson in UpdateOperation(fromJson: opJson) }
 		self.pristine = update["pristine"] as? Bool ?? false
 	}
+}
+
+struct UpdateOperation {
+    let type: UpdateOperationType
+    let n: Int
+    let lines: [[String: Any]]
+    let ln: UInt
+
+    init?(fromJson json: [String: Any]) {
+        guard
+            let json_type = json["op"] as? String,
+            let op_type = UpdateOperationType(rawValue: json_type),
+            let n = json["n"] as? Int else {
+                assertionFailure("Invalid 'op' json: \(json)")
+                return nil
+        }
+
+        self.type = op_type
+        self.n = n
+
+        switch self.type {
+        case .Insert:
+            self.ln = 0
+            guard let lines = json["lines"] as? [[String:Any]] else {
+                assertionFailure("Invalid 'op' json for '\(json_type)'. Invalid 'lines': \(json)")
+                return nil
+            }
+            self.lines = lines
+
+        case .Copy, .Update:
+            self.lines = []
+            guard let ln = json["ln"] as? UInt else {
+                assertionFailure("Invalid 'op' json for '\(json_type)'. Invalid 'ln': \(json)")
+                return nil
+            }
+
+            self.ln = ln
+
+        default:
+            self.ln = 0
+            self.lines = []
+            () // no-op
+        }
+    }
 }
